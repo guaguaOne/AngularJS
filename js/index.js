6a7e4f22e72cd786611340bf967823c247964dab
nian <1272153207@qq.com>
2017-03-21 09:28:51 +0800

    AngularJS

---
+++ js/index.js
@@ -0,0 +1,52 @@
+//document.addEventListener('plusready', function(){
+//	console.log("所有plus api都应该在此事件发生后调用，否则会出现plus is undefined。"
+	//获取所有漫画
+	angular.module('app',['ngRoute'])
+	.config(['$routeProvider',function($route){
+		$route
+		.when('/',{
+				templateUrl:'comic.html',
+				controller:function($scope,$http){
+					$http({
+						method:'POST',
+						url:'http://192.168.1.122/anjular.js/Demo7/php/comic.php'
+					}).then(function successCallback(msg){
+						// console.log(msg.data.showapi_res_body.pagebean.contentlist);
+						$scope.names=msg.data.showapi_res_body.pagebean.contentlist;	
+					},function errorCallback(msg){
+						console.log("error"+msg);
+					})
+				}
+			}
+		)
+		.when('/music',{templateUrl:'music.html'})
+		.when('/comicDetial',{templateUrl:'comic_detial.html'})
+		.otherwise({redirectTo:'/comic'});
+	}])
+	.controller('comicCtrl', function($scope,$http){
+		$scope.getDetial=function($id){
+			console.log("id="+$id);
+			$http({
+				method:'POST',
+				url:'http://192.168.1.122/anjular.js/Demo7/php/comic_detial.php',
+				data: {
+					id:$id
+				},
+				headers:{'Content-Type':'application/x-www-form-urlencoded'},
+				transformRequest:function(obj){
+					var str = [];    
+	                for (var p in obj) {    
+	                    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));    
+	                }    
+	                return str.join("&"); 
+				}
+			}).then(function successCallback(msg){
+				console.log("ok"+msg);
+			}, function errorCallback(msg){
+				console.log("error"+msg);
+			})
+			location.href="http://192.168.1.122/anjular.js/Demo7/#/comicDetial";
+		}
+	});
+	
+//});
---
+++ php/comic.php
@@ -0,0 +1,36 @@
+ <?php
+    header("Content-Type:text/html;charset=UTF-8");
+    date_default_timezone_set("PRC");
+    $showapi_appid = '33970';  
+    $showapi_secret = 'a728d5b5e6e64d94a5065dda45f23e94'; 
+    $paramArr = array(
+         'showapi_appid'=> $showapi_appid,
+         'type'=>"/category/weimanhua/gushimanhua",
+         'page'=>1
+         //添加其他参数
+    );
+     
+    //创建参数(包括签名的处理)
+    function createParam ($paramArr,$showapi_secret) {
+         $paraStr = "";
+         $signStr = "";
+         ksort($paramArr);
+         foreach ($paramArr as $key => $val) {
+             if ($key != '' && $val != '') {
+                 $signStr .= $key.$val;
+                 $paraStr .= $key.'='.urlencode($val).'&';
+             }
+         }
+         $signStr .= $showapi_secret;//排好序的参数加上secret,进行md5
+         $sign = strtolower(md5($signStr));
+         $paraStr .= 'showapi_sign='.$sign;//将md5后的值作为参数,便于服务器的效验
+         return $paraStr;
+    }
+     
+    $param = createParam($paramArr,$showapi_secret);
+    $url = 'http://route.showapi.com/958-1?'.$param; 
+    $result = file_get_contents($url);
+    // print $result.'<br>\r\n';
+    // $result = json_decode($result);
+    echo $result;
+    ?>
